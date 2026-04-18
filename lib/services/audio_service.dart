0a395f6a262edca297de4e58366b42afb8a1675b
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-19 02:46:19 +0530

    Add/update audio service

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -6,40 +6,64 @@ class AudioService {
   AudioService._internal();
 
   bool _isInitialized = false;
+  bool _isMuted = false;
 
   void init() {
     if (_isInitialized) return;
     FlameAudio.bgm.initialize();
-    FlameAudio.audioCache.prefix = 'assets/music/';
+    // Flame Audio automatically prepends 'assets/' to the prefix.
+    // So 'music/' becomes 'assets/music/'
+    FlameAudio.audioCache.prefix = 'music/';
     _isInitialized = true;
   }
 
-  void playBgm(String fileName, {double volume = 1.0}) {
+  /// Master switch to enable/disable all audio globally
+  void updateMuteState(bool muted) {
+    _isMuted = muted;
+    if (_isMuted) {
+      stopBgm();
+    }
+  }
+
+  Future<void> playBgm(String fileName, {double volume = 1.0}) async {
+    if (_isMuted) return; 
     init();
-    FlameAudio.bgm.play(fileName, volume: volume);
+    
+    try {
+      // If already playing, don't restart
+      if (FlameAudio.bgm.isPlaying) return;
+      
+      await FlameAudio.bgm.play(fileName, volume: volume);
+    } catch (e) {
+      print('BGM Play Error: $e');
+    }
   }
 
   void stopBgm() {
-    if (FlameAudio.bgm.isPlaying) {
-      FlameAudio.bgm.stop();
-    }
+    try {
+      if (FlameAudio.bgm.isPlaying) {
+        FlameAudio.bgm.stop();
+      }
+    } catch (_) {}
   }
 
   void pauseBgm() {
-    if (FlameAudio.bgm.isPlaying) {
-      FlameAudio.bgm.pause();
-    }
+    try {
+      if (FlameAudio.bgm.isPlaying) {
+        FlameAudio.bgm.pause();
+      }
+    } catch (_) {}
   }
 
   void resumeBgm() {
-    if (!FlameAudio.bgm.isPlaying) {
-      // In some versions resume might not work if fully stopped, 
-      // but Bgm handles its own state. 
+    if (_isMuted) return; 
+    try {
       FlameAudio.bgm.resume();
-    }
+    } catch (_) {}
   }
 
   void playSfx(String fileName, {double volume = 1.0}) {
+    if (_isMuted) return; // Block SFX too
     init();
     FlameAudio.play(fileName, volume: volume);
   }
